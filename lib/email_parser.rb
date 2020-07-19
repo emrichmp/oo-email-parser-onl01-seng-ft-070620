10f697f3ed90da8445dc695e5f5742ca585c34f2
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-19 22:18:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   end
   
   def parse()
-    csv_emails.split.collect do |email|
+    email_array = csv_emails.split.collect do |email|
       email.split(",")
     end
     csv_emails.flatten.uniq
